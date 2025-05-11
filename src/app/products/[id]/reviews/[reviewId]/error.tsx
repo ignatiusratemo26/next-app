51d8df34753bc2d9693de64aa7badaa5c3209355
Ignatius Ratemo <ignatiusratemo5@gmail.com>
2025-05-11 12:59:43 +0300

    Lnx ready

--- src/app/products/[id]/reviews/[reviewId]/error.tsx
+++ src/app/products/[id]/reviews/[reviewId]/error.tsx
@@ -1,10 +1,19 @@
 "use client"
-export default function ErrorBoundary({error} :{
-    error : Error
+import { useRouter } from "next/navigation";
+
+export default function ErrorBoundary({error, reset} :{
+    error : Error;
+    reset : () => void;
 }) {
+    const router = useRouter();
+    const handleReset = () => {
+        reset()
+        router.refresh()
+    }
     return (
         <div>
             <h1>Error in reviewID {error.message}</h1>
+            <button onClick={handleReset}>Try again</button>
         </div>
     )
 }
